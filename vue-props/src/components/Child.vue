abdd6843c1eb9ade9a18963a47b16b0e255e3ad4
zonezs <zonezs123@gmail.com>
2024-04-07 22:36:30 +0800

    组件数据传递的类型可以是任意类型

--- vue-props/src/components/Child.vue
+++ vue-props/src/components/Child.vue
@@ -2,20 +2,29 @@
     <h4>{{ title }}</h4>
 
     <h4>{{ dynamicDesc }}</h4>
+
+    <h4>{{ age }}</h4>
+    
+    <ul>
+        <li v-for="(item, index) of arr" :key="index"> {{ item }} </li>
+    </ul>
+
+    <h4 v-for="(value, key) of userInfo">{{ key }} : {{ value }}</h4>
+
 </template>
 
 <script>
 export default {
     data() {
         return {
-            desc: ""
+
         }
     },
-    // props 数据传递是用数组的方式来接收父级的参数，并且支持多个参数接收
+    // props 数据传递是用数组的方式来接收父级的 参数，并且支持多个参数接收
     /*
     注意事项：
     props 传递数据，只能父级传给子级，不能反其道而行之
     */
-    props: ["title", "dynamicDesc"]
+    props: ["title", "dynamicDesc", "age", "arr", "userInfo"]
 }
 </script>
--- vue-props/src/components/Parents.vue
+++ vue-props/src/components/Parents.vue
@@ -2,10 +2,17 @@
     <h4>{{ title }}</h4>
 
     <!-- 静态传递 -->
-    <Child title="this is parents data"/>
+    <Child title="this is parents data" />
 
     <!-- 通过 v-bind 动态传递 -->
-    <Child :title="dynamicDesc"/>
+    <Child :title="dynamicDesc" />
+    
+    <Child :age="age" />
+
+    <Child :arr="arr" />
+    
+    <Child :userInfo="userInfo" />
+
 </template>
 
 <script>
@@ -16,7 +23,13 @@ export default {
     data() {
         return {
             title: "父控件 Parents",
-            dynamicDesc: "父子组件的动态数据传递"
+            dynamicDesc: "父子组件的动态数据传递",
+            age: 20,
+            arr: [10, 20, 30],
+            userInfo: {
+                name: "kk",
+                age: 20
+            }
         }
     },
     components: {
