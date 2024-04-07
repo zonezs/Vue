be1c5540bbbf9b396dfe243c3a8dfc6a731174fa
zonezs <zonezs123@gmail.com>
2024-04-07 22:06:53 +0800

    父子数据传递注意事项

--- vue-props/src/components/Child.vue
+++ vue-props/src/components/Child.vue
@@ -11,6 +11,11 @@ export default {
             desc: ""
         }
     },
+    // props 数据传递是用数组的方式来接收父级的参数，并且支持多个参数接收
+    /*
+    注意事项：
+    props 传递数据，只能父级传给子级，不能反其道而行之
+    */
     props: ["title", "dynamicDesc"]
 }
 </script>
