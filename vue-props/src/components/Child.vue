4d91ef984d3cdd3b83a8784dfaaeae56ea62c893
zonezs <zonezs123@gmail.com>
2024-04-07 22:03:08 +0800

    父子传递props:["title"]

---
+++ vue-props/src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+    <h4>{{ title }}</h4>
+
+    <h4>{{ dynamicDesc }}</h4>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            desc: ""
+        }
+    },
+    props: ["title", "dynamicDesc"]
+}
+</script>
---
+++ vue-props/src/components/Parents.vue
@@ -0,0 +1,26 @@
+<template>
+    <h4>{{ title }}</h4>
+
+    <!-- 静态传递 -->
+    <Child title="this is parents data"/>
+
+    <!-- 通过 v-bind 动态传递 -->
+    <Child :title="dynamicDesc"/>
+</template>
+
+<script>
+
+import Child from './Child.vue';
+
+export default {
+    data() {
+        return {
+            title: "父控件 Parents",
+            dynamicDesc: "父子组件的动态数据传递"
+        }
+    },
+    components: {
+        Child
+    }
+}
+</script>
