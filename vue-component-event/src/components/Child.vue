818995d24a6c730464a122366756d49b2094eafe
zonezs <zonezs123@gmail.com>
2024-04-09 18:28:58 +0800

    组件事件emit

---
+++ vue-component-event/src/components/Child.vue
@@ -0,0 +1,6 @@
+<template>
+    <h3>Child</h3>
+</template>
+
+<script>
+</script>
---
+++ vue-component-event/src/components/ComponentEvent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>{{ title }}</h3>
+
+    <Child />
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+    data() {
+        return {
+            title: "组件事件 Event"
+        }
+    },
+    components: {
+        Child
+    }
+}
+</script>
