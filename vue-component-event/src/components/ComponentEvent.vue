1684d3fe9b52ba852d5b657d4fa25bdc9fd15d25
zonezs <zonezs123@gmail.com>
2024-04-11 18:31:55 +0800

    组件事件emit

--- vue-component-event/src/components/ComponentEvent.vue
+++ vue-component-event/src/components/ComponentEvent.vue
@@ -1,6 +1,8 @@
 <template>
     <h3>{{ title }}</h3>
     
+    <button @click="clickHandler">{{ btnTitle }}</button>
+
     <Child />
 </template>
 
@@ -9,11 +11,17 @@ import Child from './Child.vue'
 export default {
     data() {
         return {
-            title: "组件事件 Event"
+            title: "组件事件 Event",
+            btnTitle: "点我调用子组件"
         }
     },
     components: {
         Child
+    },
+    methods: {
+        clickHandler() {
+            console.log("调用了子组件");
+        }
     }
 }
 </script>
