dad6f2138660cf2b169a74d08fe1a6e9c0ca88bd
zonezs <zonezs123@gmail.com>
2024-04-21 11:28:10 +0800

    异步组件加载 defineAsyncComponent

---
+++ vue-component-defineAsyncComponent/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>异步组件</h3>
+
+  <component :is="tableComponent"></component>
+
+  <button @click="changedHandler">切换组件</button>
+</template>
+
+<script>
+import ComponentA from './components/ComponentA.vue';
+//import ComponentB from './components/ComponentB.vue';
+
+/**
+ * 注意：异步加载组件的方式，注意这种写法，特别重要
+ */
+import { defineAsyncComponent } from 'vue';
+const ComponentB = defineAsyncComponent(() => 
+  import('./components/ComponentB.vue')
+)
+
+export default {
+  data() {
+    return {
+      tableComponent: "ComponentA"
+    }
+  },
+  components: {
+    ComponentA,
+    ComponentB
+  },
+  methods: {
+    changedHandler() {
+      this.tableComponent = this.tableComponent === "ComponentA" ? "ComponentB" : "ComponentA"
+    }
+  }
+}
+</script>
---
+++ vue-component-defineAsyncComponent/src/components/ComponentA.vue
@@ -0,0 +1,3 @@
+<template>
+    <h4>ComponentA</h4>
+</template>
