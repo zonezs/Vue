04f6343c6ed593ff4b2e26ddd2c9fd8b219fff96
zonezs <zonezs123@gmail.com>
2024-05-04 22:57:31 +0800

    flex display attribute

--- vue-flex-display/src/App.vue
+++ vue-flex-display/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <h3>flex 布局</h3>
+
+  <ComponentFlex />
 </template>
 
 <script>
+import ComponentFlex from './components/ComponentFlex.vue'
 export default {
   data() {
     return {
       
     }
   },
+  components: {
+    ComponentFlex
+  }
 }
 </script>
---
+++ vue-flex-display/src/components/ComponentFlex.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: row;
+    justify-content:space-between;  /** 先两边贴边，再平均分配剩余空间 */
+    width: 80%;
+    height: 300px;
+    background-color: pink;
+    flex-wrap: wrap;
+    /* align-items: center; */
+    align-content: space-around;
+}
+
+div span {
+    background-color: purple;
+    width: 200px;
+    height: 100px;
+}
+</style>
+
+
+<!-- flex-direction：设置主轴还是侧轴 -->
+
+<!-- justify-content：设置主轴上的子元素排列方式 -->
+
+<!-- flex-wrap：设置子元素是否换行，默认是不换行的，只会缩小宽度 -->
+
+<!-- align-items：设置侧轴上子元素的排列方式，通常在子项为单行的时候使用 -->
+
+<!-- align-content: 设置侧轴上子元素的排列方式，用于多行 -->
