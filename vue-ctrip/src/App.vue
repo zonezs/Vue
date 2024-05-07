e0678d3008fde1db6128d772c7768cc4a2d50fd1
zonezs <zonezs123@gmail.com>
2024-05-07 22:55:04 +0800

    add ComponentBanner

--- vue-ctrip/src/App.vue
+++ vue-ctrip/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <ComponentSearch />
   <ComponentNavGird />
+  <ComponentBanner />
 </template>
 
 <script>
 import ComponentSearch from './components/ComponentSearch.vue'
 import ComponentNavGird from './components/ComponentNavGird.vue'
+import ComponentBanner from './components/ComponentBanner.vue'
 export default {
   data() {
     return {
@@ -14,7 +16,15 @@ export default {
   },
   components: {
     ComponentSearch,
-    ComponentNavGird
+    ComponentNavGird,
+    ComponentBanner
   }
 }
 </script>
+
+<!-- 
+
+  banner pic
+https://dimg04.c-ctrip.com/images/0zg0p12000cz4eu861DA5.jpg
+
+ -->
---
+++ vue-ctrip/src/components/ComponentBanner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <img @click="imgHandler" src="https://dimg04.c-ctrip.com/images/0zg0p12000cz4eu861DA5.jpg" alt="banner">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        imgHandler() {
+            console.log("img click");
+        }
+    },
+}
+</script>
+
+<style scoped> 
+div {
+    width: 100%;
+    height: 120px;
+}
+img {
+    margin-top: 10px;
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+}
+</style>
