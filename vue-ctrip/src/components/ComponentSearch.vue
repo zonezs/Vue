b14da5657a94669a768d7dcbc40d9aa5767641e6
zonezs <zonezs123@gmail.com>
2024-05-08 08:50:48 +0800

    add top blank

--- vue-ctrip/src/components/ComponentSearch.vue
+++ vue-ctrip/src/components/ComponentSearch.vue
@@ -1,20 +1,23 @@
 <template>
+    <div class="blank"></div>
     <div class="search">
-        <span></span>
         <span>搜索：目的地/酒店/景点/航班号</span>
     </div>
 </template>
 
 <style scoped>
+/* 填充顶部空白高度 */
+.blank {
+    width: 100%;
+    height: 10px;
+}
 .search {
-    margin-top: 10px;
     padding-left: 20px;
     line-height: 28px;
     border-radius: 16px;
     border: 1.6px solid #0086F6;
     height: 32px;
 }
-
 .search span {
     color: #999;
 }
