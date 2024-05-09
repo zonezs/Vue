21a93a02ce6b27283c1e0a60fbcefc643bdeeb0d
zonezs <zonezs123@gmail.com>
2024-05-09 18:45:24 +0800

    全局 axios 挂载后,使用的话采用 this.$axios 即可

--- vue-axios/src/main.js
+++ vue-axios/src/main.js
@@ -6,7 +6,7 @@ import App from "./App.vue";
 import axios from "axios"; /** 引入全局 axios */
 
 const app = createApp(App);
-app.config.globalProperties.$axios = axios; /** 挂载全局 axios */
+app.config.globalProperties.$axios = axios; /** 挂载到全局，后面使用就用 this.$axios 即可 */
 app.mount("#app");
 
 // createApp(App).mount('#app')
