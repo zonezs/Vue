0e6005b9420643f8179eba7b5ad2cdc21e6feaaf
zonezs <zonezs123@gmail.com>
2024-05-10 11:58:06 +0800

    封装 axios

---
+++ vue-axios-request/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>axios 的封装使用</h3>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    console.log('mounted');
+  },
+}
+</script>  
